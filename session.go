b8176e45151632c0c5ba20e60adf9b71ab73bb64
Marc Hoersken <info@marc-hoersken.de>
2019-05-25 18:09:17 +0200

    Drop obsolete legacy Send method from Session type

--- session.go
+++ session.go
@@ -15,20 +15,6 @@ func (s *session) Reset() {
 	s.c.Reset()
 }
 
-func (s *session) Send(from string, to []string, r io.Reader) error {
-	if err := s.Mail(from); err != nil {
-		return err
-	}
-
-	for _, rcpt := range to {
-		if err := s.Rcpt(rcpt); err != nil {
-			return err
-		}
-	}
-
-	return s.Data(r)
-}
-
 func (s *session) Mail(from string) error {
 	return s.c.Mail(from)
 }
